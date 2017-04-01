7190425e3a36d35ad8bcc97b8597b9d1f095a670
Francesco Maida <francesco.maida@gmail.com>
2017-04-01 13:40:56 +0200

    Reorganized a little bit

--- commander/exceptions/__init__.py
+++ commander/exceptions/__init__.py
@@ -1,7 +1,1 @@
-class UnknownCommand(Exception):
-    """
-    This exception handles unknown commands
-    """
-
-    def __init__(self, cmd):
-        Exception.__init__(self, "Uknown command: {}".format(cmd))
+from .unknowncommand import UnknownCommand
